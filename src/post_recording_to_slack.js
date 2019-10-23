72290142af29fa821cc21cba7cb11f9a911d5591
Dan Moore <mooreds@transposit.com>
2019-10-23 21:06:43 +0000

    pull channel from the user settings

--- src/post_recording_to_slack.js
+++ src/post_recording_to_slack.js
@@ -5,7 +5,8 @@
   const startDate = moment().add(-1, 'months').format("YYYY-MM-DD");
   const endDate = moment().format("YYYY-MM-DD");
   const recordings = api.run("this.recordings_list", {start_date : startDate, end_date: endDate});
- 
+
+  const channel = user_setting.get('channel');
   recordings.forEach(r => {
     const stashKey = userId + "-" + r.id;
     if (stash.get(stashKey)) {
@@ -13,7 +14,7 @@
     }
     const date = moment(r.start_time);
     const text = `${r.topic} was recorded on ${date.format("MMM DD, YYYY")}. Link to recording: ${r.share_url}`
-    api.run("this.post_chat_message",{text: text, channel: 'video-archive'});
+    api.run("this.post_chat_message",{text: text, channel: channel});
     stash.put(stashKey,true);
   });
   return {
